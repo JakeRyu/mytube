113878a829b601a6b44c165f0966285a052d8580
Jake Ryu <jake.ryu@hotmail.co.uk>
2020-10-10 23:33:48 +0100

    Clean up warnings

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
@@ -12,21 +12,20 @@ function App({ youtube }) {
         setSelctedVideo(video);
     };
 
-    const search = (query) => {
+    const search = useCallback((query) => {
         youtube
             .search(query) //
             .then((videos) => {
                 setVideos(videos);
                 setSelctedVideo(null);
             });
-    };
+    }, [youtube]);
 
     useEffect(() => {
         youtube
             .mostPopular() //
             .then((videos) => setVideos(videos));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [youtube]);
 
     return (
         <div className={styles.app}>
--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -4,14 +4,15 @@ import styles from "./video_detail.module.css";
 const VideoDetail = ({ video, video: { snippet } }) => (
     <section className={styles.detail}>
         <iframe
+            title="video play"
             className={styles.video}
             id="ytplayer"
             type="text/html"
             width="100%"
             height="500px"
             src={`https://www.youtube.com/embed/${video.id}`}
-            frameborder="0"
-            allowfullscreen
+            frameBorder="0"
+            allowFullScreen
         ></iframe>
         <h2>{snippet.title}</h2>
         <h3>{snippet.channelTitle}</h3>
