dd64969b84f18cf16419e27addc30d5fb8a7ff2c
Jake Ryu <jake.ryu@hotmail.co.uk>
2020-10-10 14:32:56 +0100

    Handle video click

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelctedVideo] = useState(null);
+  
+  const selectVideo = (video) => {
+    setSelctedVideo(video);
+  }
+  
   const search = query => {
     youtube
       .search(query) //
@@ -15,11 +22,16 @@ function App({ youtube }) {
     youtube
       .mostPopular() //
       .then(videos => setVideos(videos));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
-    <div className={styles.app}>
+    <div className={styles.app}>  
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      {selectedVideo &&
+        <VideoDetail video={selectedVideo} />
+      }
+      <VideoList videos={videos} onVideoClick={selectVideo} />
     </div>
   );
 }
