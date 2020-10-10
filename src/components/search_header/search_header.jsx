bf9af27cf428a87b9017b738d3720015af65d74f
Jake Ryu <jake.ryu@hotmail.co.uk>
2020-10-11 00:29:33 +0100

    Responsive mode

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -22,7 +22,7 @@ const SearchHeader = memo(
       <header className={styles.header}>
         <div className={styles.logo}>
           <img className={styles.img} src="/images/logo.png" alt="logo" />
-          <h1 className={styles.title}>Youtube</h1>
+          <h1 className={styles.title}>MyTube</h1>
         </div>
         <input
           ref={inputRef}
